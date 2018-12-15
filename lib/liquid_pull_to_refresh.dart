d64ceb4fb1c9fea8242db1b11975477163a9f008
aagarwal1012 <aagarwal9782@gmail.com>
2018-12-16 01:16:47 +0530

    added basic example app

--- lib/liquid_pull_to_refresh.dart
+++ lib/liquid_pull_to_refresh.dart
@@ -1,7 +1,8 @@
 library liquid_pull_to_refresh;
 
+import 'package:flutter/material.dart';
 /// A Calculator.
-class Calculator {
+class Calculator extends RefreshIndicator{
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
