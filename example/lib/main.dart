c7a1a8f037ce37a25b49624d80959eeaef030958
aagarwal1012 <aagarwal9782@gmail.com>
2018-12-17 00:24:45 +0530

    example app updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
@@ -27,6 +29,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+  static final List<String> _items = <String>[
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N'
+  ];
+
+  Future<void> _handleRefresh() {
+    final Completer<void> completer = Completer<void>();
+    Timer(const Duration(seconds: 3), () {
+      completer.complete();
+    });
+    return completer.future.then<void>((_) {
+      _scaffoldKey.currentState?.showSnackBar(SnackBar(
+          content: const Text('Refresh complete'),
+          action: SnackBarAction(
+              label: 'RETRY',
+              onPressed: () {
+                _refreshIndicatorKey.currentState.show();
+              })));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,8 +72,22 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: LiquidPullToRefresh(
-        child: ListView(),
-        onRefresh: (){},
+        key: _refreshIndicatorKey,
+        onRefresh: _handleRefresh,
+        child: ListView.builder(
+          padding: kMaterialListPadding,
+          itemCount: _items.length,
+          itemBuilder: (BuildContext context, int index) {
+            final String item = _items[index];
+            return ListTile(
+              isThreeLine: true,
+              leading: CircleAvatar(child: Text(item)),
+              title: Text('This item represents $item.'),
+              subtitle: const Text(
+                  'Even more additional list item information appears on line three.'),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/liquid_pull_to_refresh.dart
+++ lib/liquid_pull_to_refresh.dart
@@ -5,7 +5,33 @@ import 'package:flutter/material.dart';
 class LiquidPullToRefresh extends RefreshIndicator {
   const LiquidPullToRefresh({
     Key key,
-    Widget child,
-    RefreshCallback onRefresh,
-  }) : super(key: key, child: child, onRefresh: onRefresh);
+    @required Widget child,
+    double displacement = 40.0,
+    @required RefreshCallback onRefresh,
+    Color color,
+    Color backgroundColor,
+    ScrollNotificationPredicate notificationPredicate =
+        defaultScrollNotificationPredicate,
+    String semanticsLabel,
+    String semanticsValue,
+  }) : super(
+          key: key,
+          child: child,
+          displacement: displacement,
+          onRefresh: onRefresh,
+          color: color,
+          notificationPredicate: notificationPredicate,
+          semanticsLabel: semanticsLabel,
+          semanticsValue: semanticsValue,
+        );
+
+//  @override
+//  _LiquidPullToRefreshState createState() => _LiquidPullToRefreshState();
+}
+
+class _LiquidPullToRefreshState extends RefreshIndicatorState {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
