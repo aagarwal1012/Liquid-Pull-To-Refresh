b1f74d3611e2af13936ef3ff7b2e137ea427a7d6
aagarwal1012 <aagarwal9782@gmail.com>
2018-12-25 01:58:17 +0530

    fixed flutter analysis issues

--- lib/liquid_pull_to_refresh.dart
+++ lib/liquid_pull_to_refresh.dart
@@ -569,8 +569,9 @@ class _LiquidPullToRefreshState extends State<LiquidPullToRefresh>
     double height = (widget.height != null) ? widget.height : _defaultHeight;
 
     // converting list items to slivers
+    // ignore: invalid_use_of_protected_member
     List<Widget> slivers = List.from(widget.child.buildSlivers(context),
-        growable: true); // ignore: invalid_use_of_protected_member
+        growable: true);
 
     //Code Added for testing
 //    slivers.insert(
